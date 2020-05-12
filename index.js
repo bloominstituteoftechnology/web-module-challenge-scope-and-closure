5f74961cdcc1d76293549efe285a0959fc0d3354
Jameslambdastudent <63371643+Jameslambdastudent@users.noreply.github.com>
2020-05-12 19:05:52 -0400

    still need help on task 4

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // ⭐️ Example Challenge START ⭐️
-
 /**
  * ### Challenge `processFirstItem`
  * 
@@ -27,10 +26,22 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ * couter
+ *
+ * counter1 uses a closure. it returns a function that updates the variable in the parent function's scope.
+ 
+ counter2 increments a variable in the global scope named count.
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ * counter1 uses a closure. It returns a function that affects a variable in the scope of the function
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
+ * counter1's code might be useful if you only want the count variable to be increased by calling the inner function.
+ *That way, there's only one way that it can be increased.
+ * 
+ * counter2's code could be more preferable if you want to use the counter variable in many different functions as
+ * well as in the global scope, (logging and printing it out to the user for example.) 
  *
 */
 
@@ -56,11 +67,13 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+return Math.round(Math.random() * 2);
 }
+
+
+
+
 
 /* Task 3: finalScore()
 
@@ -76,11 +89,20 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+ function finalScore(cb, inningNum){
 
-  /*Code Here*/
+   let score ={};
 
-}
+let homeTeamGamesScore=cb*inningNum;
+let awayTeamGamesScore=cb*inningNum;
+
+score.Home = homeTeamGamesScore;
+score.Away = awayTeamGamesScore;
+
+return score;
+ };
+ finalScore(inning(), 2);
+
 
 /* Task 4: 
 
@@ -103,8 +125,7 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
-
-
+// function scoreboard() {
+  
+  
+// }
