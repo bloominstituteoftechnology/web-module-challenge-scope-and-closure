d62edb7f62af9a7ef3edf9a4c0a0341cb3ed7ed1
Coleah Mattern <coleahmattern@gmail.com>
2020-05-18 17:34:40 -0700

    all done

--- index.js
+++ index.js
@@ -26,9 +26,11 @@ function processFirstItem(stringList, callback) {
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
- * 1. What is the difference between counter1 and counter2?
+ * 1. What is the difference between counter1 and counter2? 
+ * Counter 1 defines the variable "count" inside the function
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ * Counter 1 uses a closure; the function counter is nested inside the function counterMaker.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
@@ -56,12 +58,12 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+  return Math.floor(Math.random() * 3);
 }
 
+console.log(inning())
+ 
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
@@ -76,11 +78,23 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(callback, num){
+  let home = 0;
+  let away = 0;
+  for (let i=0; i< num; i++){
+    home = home + callback();
+    away = away + callback();
+  }
+    return {
+      "Home": home,
+      "Away": away
+    };
+  }
 
+  console.log(finalScore(inning,1));
   /*Code Here*/
 
-}
+
 
 /* Task 4: 
 
@@ -102,9 +116,25 @@ and returns the score at each pont in the game, like so:
 9th inning: 6 - 10
 
 Final Score: 6 - 10 */
-
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function getInningScore(inning) {
+  return {
+      home: inning(),
+      away: inning(),
+  }
 }
 
+function scoreboard(getInningScore,inning,num) {
+  let scoreByInning = [];
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i<num; i++){
+    const currentInning = getInningScore(inning)
+    homeScore = homeScore + currentInning.home;
+    awayScore = awayScore + currentInning.away;
+    scoreByInning.push(`inning ${i+1}: Away ${currentInning.away} - Home ${currentInning.home}`)
+    //expand on this for loop//
+  }
+ return scoreByInning;
+  }
+console.log(scoreboard(getInningScore, inning, 9));
 
