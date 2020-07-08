7f2cbe6b893f219cdea8e1767c8c6055398c89eb
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-07-08 19:45:46 -0400

    Modified the function in task 2, so that it returns only the points

--- index.js
+++ index.js
@@ -64,12 +64,12 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(team){
+function inning(){
   let score = Math.round(Math.random() * 2);
-  return `Team ${team} has a scored ${score} points!`; 
+  return score; 
 };
 
-console.log(inning("Braves"));
+console.log(inning())
 
 
 /* Task 3: finalScore()
