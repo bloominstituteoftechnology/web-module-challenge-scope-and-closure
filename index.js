6381e18da0b411ec0435e043718e0a988fb1418f
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-04 04:17:39 -0700

    innings function

--- index.js
+++ index.js
@@ -27,44 +27,54 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ *    The main difference is that closure never goes outside its function  in counter 1 while counter2s closure goes 
+ *    outisde the function's scope   
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ *    counter2 because it maniupulating the count variable outisde the function's scope
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ *    Counter 1 code would be quicker and more light weight. It also has the option of making count assignable as a parameter which 
+ *    would be more structurized
+ * 
+ *    The counter2 code would be good if there was variables on the global scope we needed to access. It would not run as fast though without 
+ *    a callback function and the same variable would not be accessible as a parameter with its global closure
 */
 
 // counter1 code
 function counterMaker() {
-  let count = 0;
-  return function counter() {
-   return count++;
-  }
+    let count = 0;
+    return function counter() {
+    return count++;
+    }
 }
 
 const counter1 = counterMaker();
-
+console.log(counter1());
 // counter2 code
 let count = 0;
 
 function counter2() {
-  return count++;
+   return count++;
 }
 
 
 /* Task 2: inning() 
 
-Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
+Write a function called `inning` that returns a random number of points that a team scored in an inning. 
+This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+   return Math.floor(Math.random() * Math.floor(3));
 
 }
+console.log(inning());
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function 
+`inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -76,9 +86,23 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning,innings){
+    let finalScoreOfGame = {Home:0,Away:0};
+    if(innings < 9){
+      // Recursivly increase finalScore function until one of the Teams win within 8 innings
+    }else if(innings === 9){
+      // Assert there is no tie
+        if(finalScoreOfGame.Home === finalScoreOfGame.Away){
+            // Extra innigs Keep increasing innings until final Score is not tied & a team won
 
-  /*Code Here*/
+        }else{
+          return finalScoreOfGame;
+        }
+    }
+
+    return finalScoreOfGame;
+
+  
 
 }
 
@@ -103,8 +127,8 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard() {
+ 
 }
 
 
