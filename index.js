76486c877c5e24018978398015b95aa72cae21b9
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-08-05 09:56:57 -0700

    task2

--- index.js
+++ index.js
@@ -59,11 +59,12 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
-}
+function inning(team){
+  let atBat = Math.floor(Math.random() * 3);
+  console.log(`${team} scored ${atBat} this inning`)
+  
+  }
+  inning("teamA");
 
 /* Task 3: finalScore()
 
