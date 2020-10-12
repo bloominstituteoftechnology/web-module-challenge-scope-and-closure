6dbb7a073ebf8536b5dc123679e5c07bd66a32db
Cameron Mirza <cmirza@Camerons-MBP.lan>
2020-10-12 15:39:51 -0700

    completed mvp

--- index.js
+++ index.js
@@ -28,9 +28,15 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ *    counter1 'count' variable is in the local scope the counterMaker function. counter2 'count' variable is in the global scope.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ *    counter1, it has access to the count variable inside the parent function.
+ * 
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ * 
+ *    counter1 is preferable when you want to reuse the counterMaker function and have seprate count variables. count2 would be better if you only need to use the function once and want just one variable.
  *
 */
 
@@ -56,9 +62,9 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+    return Math.floor(Math.random()*3);
 
 }
 
@@ -76,11 +82,26 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inputFunction,inputInnings){    // finalScore takes an input function and an input innings
 
-  /*Code Here*/
+  let score = [];                                   // initialize score array for return object
+  let away = 0;                                     // initialize away score variable
+  let home = 0;                                     // initialize home score variable
 
+  for (let i=0; i<inputInnings; i++){               // run a for loop that runs as long as the number taken in the input innings
+    away+=inputFunction();                          // get a random score from the input function and add it to away variable
+    home+=inputFunction();                          // get a random score from the input function and add it to home variable
+  }
+
+  score.push({                                      // push the home and away scores to the score array in the form of an object
+    Home: inputFunction(),
+    Away: inputFunction()
+  })
+
+  return score;                                     // return the score object
 }
+
+console.log(finalScore(inning,9))
 
 /* Task 4: 
 
@@ -103,8 +124,30 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function getInningScore(inputInning, awayScore, homeScore){
+  if(inputInning === 1){
+    console.log(`1st inning: ${awayScore} - ${homeScore}`)
+  } else if(inputInning === 2){
+    console.log(`2nd inning: ${awayScore} - ${homeScore}`)
+  }else if(inputInning === 3){
+    console.log(`3rd inning: ${awayScore} - ${homeScore}`)
+  }else{
+    console.log(`${inputInning}th inning: ${awayScore} - ${homeScore}`)
+  }
 }
 
+function scoreboard(inputInningScore, showInningScore, numOfInnings) {
+  let homeScore=0;
+  let awayScore=0;
 
+  for(let inningNumber=1; inningNumber<=numOfInnings; inningNumber++){
+    let homeRunsScored=inputInningScore();
+    homeScore=homeScore+homeRunsScored;
+    let awayRunsScored=inputInningScore();
+    awayScore=awayScore+awayRunsScored;
+    showInningScore(inningNumber, awayScore, homeScore);
+  }
+  return `Final Score: ${awayScore} - ${homeScore}`
+}
+
+console.log(scoreboard(inning, getInningScore, 9))
