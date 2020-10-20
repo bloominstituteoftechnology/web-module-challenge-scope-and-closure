8371366ab6567f5f89b629c524582e9d332d3f82
Vital Boisset <vital.boisset@gmail.com>
2020-10-20 19:10:11 -0400

    Completed all 4 tasks

--- index.js
+++ index.js
@@ -2,44 +2,49 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *
+ *  counter 1 has a locally declared 'count' variable, counter 2 has a globally declared 'count' variable.
+ *
+ *
+ * 2. Which of the two uses a closure? How can you tell?
+ *
+ * counter1, because it uses a function within another function and declares
+ *
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ * counter1
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,16 +56,16 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(teamScored) {
+  var teamScored = Math.round(Math.random() * 2);
+  return teamScored;
 }
+
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -74,13 +79,19 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inning, numberOfInnings) {
+  var awayScore = 0;
+  var homeScore = 0;
+  for (let i = 0; i < numberOfInnings; i++) {
+    awayScore += inning();
+    homeScore += inning();
+  }
+  return `"Home": ${homeScore},\n"Away": ${awayScore},`;
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -102,9 +113,54 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
+//function getInningScore(awayScore, homeScore) {
+//  return awayScore + " - " + homeScore;
+//}
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+//function scoreboard(getInningScore, inning, numberOfInnings) {
+//  var awayScore;
+//  var homeScore;
+//  var currentInning = 0;
+
+// for (let i = 0; i < numberOfInnings; i++) {
+//    awayScore = inning();
+//    homeScore = inning();
+//    if (currentInning === 1) {
+//      currentInning + "st inning: " + awayScore + " - " + homeScore;
+//    } else if (currentInning === 2) {
+//    }
+//    return `${currentInning} ${homeScore},\n"Away": ${awayScore},`;
+//  }
+//}
+
+//console.log(scoreboard(getInningScore, inning, 9));
+//how many innings?
+//what's the score for inning 1?
+//display score for 1st inning with proper labeeling
+//repeat for all innnings
+//display final score
+
+//calculate awayTeam and homeTeam scores for inning 1
+//increment final score variable
+//display inning#, awayTeam and homeTeam scores
+//repeat for N innings
+
+//scoreboard
+
+//  Number of innings
+
+//        inning score
+
+function scoreboard(inning, innings) {
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i=1 ; i <= innings ; i++) {
+    homeScore += inning();
+    awayScore += inning();
+    console.log(`Inning ${i} | Home: ${homeScore} | Away: ${awayScore}`);
+    console.log(`Final Score | Home: ${homeScore} | Away: ${awayScore}`);
+  }
 }
 
-
+scoreboard(inning, 9);
