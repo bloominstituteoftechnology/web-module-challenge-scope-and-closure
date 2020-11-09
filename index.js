8418e9ab075b9dad11129020fb56b0217d17770a
Michelle Chen <mchen0062@gmail.com>
2020-11-09 16:39:47 -0600

    task 4 & 5 left

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(processFirstItem(['foo','bar'],function(str){return str+str})); //another function was created as the 'callBack' argument
 
 // ⭐️ Example Challenge END ⭐️
 
@@ -28,11 +28,15 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  In counter 1, the counter is stored in a function, and the count is updated within the function, but everything remains in the function scope and only the counterMaker
+  function is accessible. The second counter is a function that counts in increments and updates the count variable.
   
   2. Which of the two uses a closure? How can you tell?
-  
+  the first counter uses a closure because there is a function nested within the function. 
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better? Counter 1 may be preferable when you want to use a counter within a function, and counter2 may be preferable when you want to use a counter
+     in a global scale. 
 */
 
 // counter1 code
@@ -61,11 +65,9 @@ Use the inning function below to do the following:
   For example: invoking inning() should return a numerical score value of 0, 1, or 2
 */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
-}
+function inning(){
+  return Math.floor(Math.random() * Math.floor(2));
+} 
 
 /* Task 3: finalScore()
 Use the finalScore function below to do the following:
@@ -80,20 +82,13 @@ For example: invoking finalScore(inning, 9) might return this object:
 }
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
-}
 
 /* Task 4: 
 // create a function called getInningScore 
 // the function should take the inning function as an argument 
-// it should return an object with with a score for home and a score for away that that populates from invoking the inning callback. */
+// it should return an object with a score for home and a score for away that populates from invoking the inning callback. */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-}
+
 /* Task 5: scoreboard()
 Use the scoreboard function below to do the following:
   1. Receive a callback function, that you create, called `getInningScore`
