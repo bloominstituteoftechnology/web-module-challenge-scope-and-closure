c15b5cba1f606b8b9678f3ef4b29950ef6370132
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2020-12-11 17:27:38 -0800

    firstSave

--- index.js
+++ index.js
@@ -62,10 +62,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
-}
-
+function inning(){
+  return Math.floor(Math.random()* Math.floor(3));
+  }
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
