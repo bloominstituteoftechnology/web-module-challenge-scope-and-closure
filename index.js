418ecc54392e8f1ab76bb2a25d4469d9262354d6
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2020-12-14 20:01:14 -0500

    Task 4 completed

--- index.js
+++ index.js
@@ -128,8 +128,21 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+let homeScore=0;
+let awayScore=0;
+function getInningScore(taskFourCallback) {
+  let FourScoreboard={
+    Home: homeScore+taskFourCallback(), //need to store the value for future use. Maybe try nesting a function here? Or storing outside?
+    Away: awayScore+taskFourCallback()
+  }
+  return FourScoreboard
+
+  /* attempt 1
+  let homeScore=0
+  let awayScore=0
+  const addInningScore=taskFourCallback(){
+    return 
+  } */
 }
 
 
