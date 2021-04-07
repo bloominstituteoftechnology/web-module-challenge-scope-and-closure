bcd19235324d04ff4cd5b27fe5a9c9509e430d1f
Paige Hicks <pnhicks1@gmail.com>
2021-04-06 23:41:57 -0400

    task1 complete

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    - counter1 is using closure and counter2 is using a function to update the value inside of it
   2. Which of the two uses a closure? How can you tell?
-  
+    - counter1. It uses the first function to declare the initial count of 0.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better? 
+    - not really sure.  *reminder to ask in class*
 */
 
 // counter1 code
