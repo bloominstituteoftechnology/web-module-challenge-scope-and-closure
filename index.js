11e6b2abe20130aa4094576682d1b4665313f550
Marshal Wahl <57955945+mwahl1@users.noreply.github.com>
2021-04-08 00:08:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2?
+  1. What is the difference between counter1 and counter2? counter 1 we have 2 clousres referenced and in 2 we have 1. 
   
-  2. Which of the two uses a closure? How can you tell?
+  2. Which of the two uses a closure? How can you tell? return can be the closures. its referenced inside the scope
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?  counter 2 is written smoother and more concsie. 
 */
 
 // counter1 code
@@ -62,9 +62,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(max){
+    return Math.floor(Math.random() * 3)
 }
+
+console.log(inning())
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,7 +83,7 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning){
   /*Code Here*/
 }
 
