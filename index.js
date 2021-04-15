b22c0e26bd8a19ed19d150cbe2217138292ad7f7
cFoschini <cjfoschini25@gmail.com>
2021-04-14 20:45:16 -0400

    finished

--- index.js
+++ index.js
@@ -29,10 +29,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+  Counter 1 is using function scope while Counter 2 is using global
+
   2. Which of the two uses a closure? How can you tell?
   
+Counter 2, The variable is not declared inside the function.
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?  Counter 1 resets, while Counter 2 does not 
 */
 
 // counter1 code
@@ -62,9 +66,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    const min = 0;
+    const max = 2;
+
+    return Math.floor(Math.random()) * (max - min + 1) + min;
 }
+
+inning()
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,18 +90,25 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning){
+  for (i=0; i<3; i++){
+    console.log(home[i])
+    console.log(away[i])
+    return finalScore;
+  }
 }
+finaleScore(inning, 9)
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  return homeScore;
+  return awayScore;
 }
+getInningScore(inning)
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
