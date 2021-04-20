25b9669caee8d834cd8a53cd4c1f980b49775970
Zulu Bravo <zulubravo@zulus-mbp.myfiosgateway.com>
2021-04-19 20:22:25 -0400

    finished task 4

--- index.js
+++ index.js
@@ -107,9 +107,13 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCb) {
+  return {
+    Home: inningCb(),
+    Away: inningCb(),
+  };
 }
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
