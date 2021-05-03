fe2a55f11565a8c76bb25fa843aedb07795470eb
David Spear <davidspear@Davids-MacBook-Pro.local>
2021-05-03 03:05:37 -0400

    First commit

--- index.js
+++ index.js
@@ -102,7 +102,8 @@ Use the scoreboard function below to do the following:
   3. Receive a number of innings to be played
   4. Return an array where each of it's index values equals a string stating the
   Home and Away team's scores for each inning.  Not the cummulative score.
-  5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
+  5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  
+  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
      If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
   
   NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
