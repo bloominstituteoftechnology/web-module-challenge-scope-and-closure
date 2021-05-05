f7858ff12df441a1706dd406556404d031fa15be
Eric <Eric@erics-air.lan>
2021-05-05 16:46:59 -0500

    completed Task 2

--- index.js
+++ index.js
@@ -62,9 +62,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * 3)
 }
+ console.log(inning);
+
+
+
+
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
