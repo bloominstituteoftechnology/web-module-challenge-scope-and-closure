b1e8cd4632d10b85a09189dee6522d8132f3573e
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-05-05 02:42:51 -0500

    edit

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+
+      counter1 = countMaker and counter2 = count
+
   2. Which of the two uses a closure? How can you tell?
+
+    The first function uses closure because it sets counter1 to the function countMaker.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+    counter1 would be preferable if there is no need to redefine or redeclare counter1. counter2 would be better if there was a need to redefine counter2. 
+
 */
 
 // counter1 code
@@ -62,8 +69,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * Math.floor(2))
 }
 
 
@@ -81,19 +88,22 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning){
+  return {
+    Home: inning(),
+    Away: inning()
+  }
 }
+console.log(finalScore(inning))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(/* CODE HERE */) {
+      /*CODE HERE*/
 }
-
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
