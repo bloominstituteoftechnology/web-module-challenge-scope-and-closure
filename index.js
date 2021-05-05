d2bd53ba1c4cda596df575cc279a8c01a8c33c65
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-05-04 22:00:43 -0400

    First Commit

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+      - Counter 1 is a local scope and Counter 2 is a global scope.
   
   2. Which of the two uses a closure? How can you tell?
+
+      - Counter 2 uses a closure because the variable is outside of the function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+      - Counter 1 has the reset and would be preferable in. Counter 2 would be better for 
 */
 
 // counter1 code
@@ -62,8 +68,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  var minimum = 0;
+  var maximum = 2;
+  return value.finalScore(value.random()) * 
+  
 }
 
 
@@ -81,8 +90,12 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning) {
+  for (i = 0; i++){
+    console.log(homeTeam[i]);
+    console.log(awayTeam[i]);
+    return finalScore
+  }
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,9 +103,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  return homeTeam;
+  return awayTeam;
 }
+
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
