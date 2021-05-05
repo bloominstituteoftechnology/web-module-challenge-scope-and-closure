1b117edb4e7fe8ade965db039581b742ea0e2f64
Andrew Brough <broughsef@gmail.com>
2021-05-05 17:52:33 -0600

    finished 1-3

--- index.js
+++ index.js
@@ -29,10 +29,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+  Counter 2 is using the global scope while counter 1 is not.
+  
   2. Which of the two uses a closure? How can you tell?
+
+  Counter 2, because the variable is declared after the function
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     Counter 1 would be better in the case that you need to reset the function
 */
 
 // counter1 code
@@ -62,11 +68,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+  const intOne = 0;
+  const intTwo = 2;
+  return Math.floor(Math.random()) * (2)
 }
-
-
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
   1. Receive the callback function `inning` that was created in Task 2 
@@ -81,9 +87,20 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, intThree){
+  var home = 0;
+  var away = 0;
+  for (let i = 0; i <= intThree; i++) {
+    home = home + inning();
+    away = away + inning();
+  }
+  return {
+    Home: home,
+    Away: away,
+  }
 }
+console.log(finalScore)
+finalScore(inning, 9)
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -91,8 +108,9 @@ Use the getInningScore() function below to do the following:
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
 function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-}
+ 
+  }
+
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
