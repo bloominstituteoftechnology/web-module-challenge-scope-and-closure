a1da192a02283989228f0696a4f5f90434bcd7d1
Orlando Williams <87622342+Nowaiihozay@users.noreply.github.com>
2021-09-13 18:33:08 -0400

    passed one test

--- index.js
+++ index.js
@@ -62,10 +62,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+   return Math.floor(Math.random() * Math.floor(3));
 }
 
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
